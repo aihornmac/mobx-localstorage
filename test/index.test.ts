0ffd879dd734fd70932dd75cbce0ae70242cf344
Aihorn Mac <aihornmac@ihawn.com>
2019-02-01 01:32:28 +0800

    test: add some more tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,18 @@
 import { configure } from 'mobx'
+import rls from 'reactive-localstorage'
+import * as uuidv4 from 'uuid/v4'
 import localstorage from '../src'
 
 configure({ enforceActions: 'always' })
 
 describe('enforceActions: always', () => {
+  it('feeds should work fine', () => {
+    rls.feed('a', uuidv4())
+  })
+
   it('set should work fine', () => {
-    localstorage.setItem('a', 'b')
-    localstorage.set('a', 'b')
+    localstorage.setItem('a', uuidv4())
+    localstorage.set('a', uuidv4())
   })
 
   it('delete should work fine', () => {
