bc0618c8087c7ec77907d32eab3c34e23038bb92
Aihorn Mac <aihornmac@ihawn.com>
2019-12-24 17:56:59 +0800

    feat: let user configure parser/stringfier

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,23 @@ import localStorage from 'reactive-localstorage'
 
 const referenceEnhancer = observable.map({}, { deep: false }).enhancer
 
+export interface LocalStorageConfigure {
+  readonly parser?: (data: string | null) => unknown
+  readonly stringify?: (data: unknown) => string
+}
+
 export class LocalStorage extends ObservableMap<string> implements Storage {
+  private _parseValue: (data: string | null) => unknown
+  private _stringifyValue: (data: unknown) => string
+
   constructor() {
     super(undefined, referenceEnhancer, 'LocalStorage')
-    const len = localStorage.length
-    for (let i = 0; i < len; i++) {
-      const key = localStorage.key(i)!
-      const value = parseValue(localStorage.getItem(key))
-      super.set(key, value)
-    }
+    this._parseValue = parseValue
+    this._stringifyValue = x => JSON.stringify(x)
+    this._reload()
     localStorage.on('change', action((key: string, value: string | null) => {
       if (typeof value === 'string') {
-        super.set(key, parseValue(value))
+        super.set(key, this._parseValue(value))
       } else {
         super.delete(key)
       }
@@ -49,7 +54,7 @@ export class LocalStorage extends ObservableMap<string> implements Storage {
 
   @action
   set(key: string, value?: any) {
-    localStorage.setItem(key, JSON.stringify(toJS(value)))
+    localStorage.setItem(key, this._stringifyValue(value))
     return this
   }
 
@@ -59,15 +64,44 @@ export class LocalStorage extends ObservableMap<string> implements Storage {
     localStorage.removeItem(key)
     return has
   }
+
+  @action
+  configure(config: LocalStorageConfigure) {
+    let changed = false
+    if (typeof config.parser === 'function') {
+      changed = true
+      this._parseValue = config.parser
+    }
+    if (typeof config.stringify === 'function') {
+      changed = true
+      this._stringifyValue = config.stringify
+    }
+    if (changed) {
+      this._reload()
+    }
+  }
+
+  private _reload() {
+    const len = localStorage.length
+    for (let i = 0; i < len; i++) {
+      const key = localStorage.key(i)!
+      const value = this._parseValue(localStorage.getItem(key))
+      super.set(key, value)
+    }
+  }
 }
 
-export function parseValue(value: any) {
+export function parseValue(value: string | null) {
   try {
-    return JSON.parse(value)
+    return value === null ? null : JSON.parse(value)
   } catch (e) {
     // ignore if cannot parse as JSON
   }
   return value
+}
+
+export function stringifyValue(value: unknown) {
+  return JSON.stringify(toJS(value))
 }
 
 export default new LocalStorage()
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,26 +1,62 @@
-import { configure } from 'mobx'
+import { expect } from 'chai'
+import { configure, toJS } from 'mobx'
 import rls from 'reactive-localstorage'
 import * as uuidv4 from 'uuid/v4'
-import localstorage from '../src'
+import localstorage, { parseValue, stringifyValue } from '../src'
 
 configure({ enforceActions: 'always' })
 
 describe('enforceActions: always', () => {
   it('feeds should work fine', () => {
     rls.feed('a', uuidv4())
+
+    expect(localstorage.has('a')).to.be.true
   })
 
   it('set should work fine', () => {
-    localstorage.setItem('a', uuidv4())
-    localstorage.set('a', uuidv4())
+    const id1 = uuidv4()
+    const id2 = uuidv4()
+    localstorage.setItem('a', id1)
+    localstorage.set('a', id2)
+
+    expect(id1).to.not.equal(id2)
+    expect(localstorage.getItem('a')).to.equal(id2)
+    expect(localstorage.getItem('a')).to.equal(localstorage.get('a'))
   })
 
   it('delete should work fine', () => {
     localstorage.removeItem('a')
     localstorage.delete('a')
+
+    expect(localstorage.has('a')).to.be.false
+    expect(localstorage.get('a')).to.be.undefined
+    expect(localstorage.getItem('a')).to.be.null
   })
 
   it('clear should work fine', () => {
     localstorage.clear()
+
+    expect(localstorage.length).to.equal(0)
+  })
+
+  it('configure should work fine', () => {
+    const key = uuidv4()
+    localstorage.setItem(key, {})
+
+    expect(localstorage.get(key)).to.deep.equal({})
+
+    localstorage.configure({
+      parser: x => x,
+      stringify: x => String(x),
+    })
+    try {
+      expect(localstorage.get(key)).to.deep.equal('{}')
+    } finally {
+      localstorage.configure({
+        parser: parseValue,
+        stringify: stringifyValue,
+      })
+      expect(localstorage.get(key)).to.deep.equal({})
+    }
   })
 })
